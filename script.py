0ad727b82e5a315efba7f43f031b7651b9106b0e
Sajad <sajadrahimi0101@gmail.com>
2023-08-29 12:17:17 +0330

    Update at 2023-08-29 12:17:17

--- script.py
+++ script.py
@@ -2,18 +2,20 @@ import datetime
 import subprocess
 import os
 
-# Get current date and time
-now = datetime.datetime.now()
-datetime_str = now.strftime("%Y-%m-%d %H:%M:%S")
+while(True):
+    # Get current date and time
+    now = datetime.datetime.now()
+    datetime_str = now.strftime("%Y-%m-%d %H:%M:%S")
 
-# Write date and time to file
-with open('LAST_UPDATED', 'w') as f:
-    f.write(datetime_str)
+    # Write date and time to file
+    with open('LAST_UPDATED', 'w') as f:
+        f.write(datetime_str)
 
-# Stage and commit changes
-subprocess.run(['git', 'add', '.'])
-subprocess.run(['git', 'commit', '-m', f'Update at {now.strftime("%Y-%m-%d %H:%M:%S")}'])
+    # Stage and commit changes
+    subprocess.run(['git', 'add', '.'])
+    subprocess.run(
+        ['git', 'commit', '-m', f'Update at {now.strftime("%Y-%m-%d %H:%M:%S")}'])
 
 
-# Push changes to GitHub
-subprocess.run(['git', 'push'])
+    # Push changes to GitHub
+    subprocess.run(['git', 'push'])
